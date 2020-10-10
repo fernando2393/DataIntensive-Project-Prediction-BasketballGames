79ecae7bc2598268b50cad9f818b6314f4fb250b
fernando2393 <garciasanzfernando2393@gmail.com>
2020-10-10 14:44:41 +0200

    game_data.json extracted.

--- ApiRequest.py
+++ ApiRequest.py
@@ -16,18 +16,22 @@ json_file = data.json()
 print(json_file['meta'])
 
 data_list = []
-for i in range(1, 487):
-    if i % (MAX_CALLS + 1) != 0:
-        print("Call: " + str(i))
-        parameters = {
-            "per_page": 100,
-            "page": i,
-        }
+for i in range(json_file['meta']['total_pages']):
+    parameters = {
+        "per_page": 100,
+        "page": i,
+    }
+    if i == 0 or i % MAX_CALLS != 0:  # Inside the available api request window
+        print("Request: " + str(i))
         aux = session.get("https://www.balldontlie.io/api/v1/games", params=parameters)
         assert aux.status_code == 200, "Status code is " + str(aux.status_code)
         data_list.append(aux.json())
     else:
         time.sleep(60)  # Wait for one minute and do other 60 API requests
+        print("Request: " + str(i))
+        aux = session.get("https://www.balldontlie.io/api/v1/games", params=parameters)
+        assert aux.status_code == 200, "Status code is " + str(aux.status_code)
+        data_list.append(aux.json())
 
 with open('game_data.json', 'w') as f:
     json.dump(data_list, f)
