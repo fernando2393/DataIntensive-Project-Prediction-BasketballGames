cb50e78f86f72334d89cd7d627f100dcbd386f6b
fernando2393 <garciasanzfernando2393@gmail.com>
2020-10-20 17:36:05 +0200

    Season averages for all rosters obtained.

--- rosterLoader.py
+++ rosterLoader.py
@@ -1,42 +1,59 @@
 import glob
 import json
 import requests
-import pandas as pd
+import os
+from time import sleep
 
-PATH = "test/"
+PATH = "roster/"
 
 
 def get_rosters(season):
-    files = glob.glob(PATH+'**/*.json', recursive=True)
+    files = glob.glob(PATH + str(season) + "/" + '**/*.json', recursive=True)
     rosters = {}
     for file in files:
         with open(file) as f:
-            if file.split("/")[1] == str(season):
-                data_list = json.load(f)
-                aux = rosters.get(data_list['team_id'])
-                if aux is not None:
-                    aux.append(data_list['player_id'])
-                    rosters[data_list['team_id']] = aux
-                else:
-                    rosters[data_list['team_id']] = [data_list['player_id']]
+            data_list = json.load(f)
+            aux = rosters.get(data_list['Team_id'])
+            if aux is not None:
+                aux.append(data_list['Player_id'])
+                rosters[data_list['Team_id']] = aux
+            else:
+                rosters[data_list['Team_id']] = [data_list['Player_id']]
 
     return rosters
 
 
 def main():
     session = requests.Session()
-    season = 288
-    rosters = get_rosters(season)
-    values = rosters[23]
-    values.sort()
-    uri = "https://www.balldontlie.io/api/v1/season_averages?season=2018"
-    for idx in values:
-        uri += "&player_ids[]="+str(idx)
-    print(uri)
-    data = session.get(uri)
-    print(data.status_code)
-    json_file = data.json()
-    print(json_file)
+    cnt = 0
+    for season in range(2002, 2016):
+        for team_id in range(1, 31):
+            rosters = get_rosters(season)
+            values = rosters.get(team_id)
+            if values is None:
+                continue
+            values.sort()
+            url = "https://www.balldontlie.io/api/v1/season_averages?season="+str(season)
+            for idx in values:
+                url += "&player_ids[]=" + str(idx)
+            data = session.get(url)
+            if data.status_code != 200:
+                print(data.status_code)
+            json_file = data.json()
+            try:
+                os.stat("season_averages/" + str(season) + "/")
+            except:
+                try:
+                    os.mkdir("season_averages/" + str(season) + "/")
+                except:
+                    os.mkdir("season_averages/")
+                    os.mkdir("season_averages/" + str(season) + "/")
+            with open("season_averages/" + str(season) + "/" + str(team_id) + ".json", 'w') as f:
+                json.dump(json_file, f)
+                print("Season " + str(season) + "\tTeam " + str(team_id) + " saved")
+                cnt += 1
+            if cnt % 60 == 0 and cnt != 0:
+                sleep(60)
 
 
 if __name__ == "__main__":
