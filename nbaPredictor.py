a87af0323df01f819f3479f15ca393679cdb60ef
fernando2393 <garciasanzfernando2393@gmail.com>
2020-10-22 00:53:31 +0200

    ML models implemented.

--- nbaPredictor.py
+++ nbaPredictor.py
@@ -1,8 +1,11 @@
 import glob
 import pandas as pd
 import numpy as np
+import os
 from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 from tqdm import tqdm
 from Features import Stats as st
 from Features import Games as gm
@@ -53,8 +56,10 @@ def gameLoader(season):
 def dataLoader(season_start, season_end, features):
     x = list()
     y = list()
+    seasons = list()
     for season in range(season_start, season_end + 1):
         games = gameLoader(season)
+        seasons.append(season)
         for game in tqdm(games):
             local_team, visitor_team = teamLoader(int(game[gm.home_team_id.value]),
                                                   int(game[gm.visitor_team_id.value]),
@@ -63,40 +68,55 @@ def dataLoader(season_start, season_end, features):
                                                   True)
             x.append(np.vstack((local_team, visitor_team)))
             y.append(np.argmax([game[gm.home_team_score.value], game[gm.visitor_team_score.value]]))
+    print("Seasons obtained " + str(seasons))
 
     return x, y
 
 
 def main():
     # Training data
-    features = [st.ast.value,
+    features = [
+                st.ast.value,
                 st.blk.value,
                 st.dreb.value,
-                st.fg3_pct.value,
+                # st.fg3_pct.value,
                 st.fg3a.value,
                 st.fg3m.value,
-                st.fg_pct.value,
+                # st.fg_pct.value,
                 st.fga.value,
                 st.fgm.value,
-                st.ft_pct.value,
+                # st.ft_pct.value,
                 st.fta.value,
                 st.ftm.value,
                 st.games_played.value,
-                st.seconds.value,
+                # st.seconds.value,
                 st.oreb.value,
                 st.pf.value,
-                st.player_id.value,
+                # st.player_id.value,
                 st.pts.value,
-                st.reb.value,
-                st.season.value,
+                # st.reb.value,
+                # st.season.value,
                 st.stl.value,
-                st.turnover.value]
+                st.turnover.value
+    ]
     print("Loading training data...")
-    x_train, y_train = dataLoader(2002, 2015, features)
+    if os.path.exists("train.npz"):
+        npz_file = np.load("train.npz")
+        x_train = npz_file['x']
+        y_train = npz_file['y']
+    else:
+        x_train, y_train = dataLoader(2002, 2014, features)  # Both initial and end are included
+        np.savez("train.npz", x=x_train, y=y_train)
     training_samples = len(x_train)
     # Testing data
     print("Loading testing data...")
-    x_test, y_test = dataLoader(2015, 2015, features)
+    if os.path.exists("test.npz"):
+        npz_file = np.load("test.npz")
+        x_test = npz_file['x']
+        y_test = npz_file['y']
+    else:
+        x_test, y_test = dataLoader(2015, 2015, features)  # Both initial and end are included
+        np.savez("test.npz", x=x_test, y=y_test)
     testing_samples = len(x_test)
     # Scale training data
     x_to_scale = np.vstack(x_train)
@@ -112,8 +132,19 @@ def main():
     x_test = [x_test[i * 24:24 * (i + 1), :] for i in range(testing_samples)]
     x_test = [x_test[i].flatten() for i in range(len(x_test))]
 
-    clf = MLPClassifier(hidden_layer_sizes=(10, 10, 5), early_stopping=True).fit(x_train, y_train)
-    print(clf.score(x_test, y_test))
+    clf = MLPClassifier(hidden_layer_sizes=(40, 28, 10), early_stopping=True, max_iter=250,
+                        solver="adam",
+                        activation="logistic"
+                        ).fit(x_train, y_train)
+    print("MLP: Accuracy on testing: " + str(clf.score(x_test, y_test)))
+
+    # clf = SVC()
+    # clf.fit(x_train, y_train)
+    # print("SVM\tAccuracy on testing: " + str(clf.score(x_test, y_test)))
+
+    clf = RandomForestClassifier(max_depth=2, random_state=0)
+    clf.fit(x_train, y_train)
+    print("Random Forest: Accuracy on testing: " + str(clf.score(x_test, y_test)))
 
 
 if __name__ == "__main__":
